fcd84e99d0b4973a51f704c4dcfd681907de018d
yr lee <info@cellumed.co.kr>
2018-02-01 10:03:22 +0900

    첫 커밋

--- app/src/main/java/com/example/lyr/gittest3/Main2Activity.java
+++ app/src/main/java/com/example/lyr/gittest3/Main2Activity.java
@@ -2,16 +2,12 @@ package com.example.lyr.gittest3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView aa = (TextView)findViewById(R.id.aa);
-        aa.setText("1111");
+        setContentView(R.layout.activity_main2);
     }
 }
-
